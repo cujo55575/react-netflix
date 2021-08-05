27c8c7e5f348486ec7b25e06b1b1ad9a58bc7463
cujo55575 <cujo55575@gmail.com>
2021-08-05 21:34:09 +0630

    frist commit

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -19,11 +19,7 @@ Player.Video = function PlayerVideo({ src, ...restProps }) {
 
   return showPlayer
     ? ReactDOM.createPortal(
-        <Overlay
-          {...restProps}
-          onClick={() => setShowPlayer(false)}
-          data-testid="player"
-        >
+        <Overlay {...restProps} onClick={() => setShowPlayer(false)}>
           <Inner>
             <video id="netflix" controls>
               <source src={src} type="video/mp4" />
